f5717a1f8aee1b5a9b9eb5ceb4584963ccc7c9e4
Shakhzod <forworkingappsinmylaptop@gmail.com>
2023-12-11 09:15:10 +0500

    my first next app

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCard from "./AddToCard";
+
+const ProductCard = () => {
+  return (
+    <div className="p-5 my-5 bg-sky-400 ">
+      <AddToCard />
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ app/users/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`, {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h3>Users</h3>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;
